d131638cd0232e642c01298078f68155a497b55d
macY <daliyjb@163.com>
2019-01-06 22:49:52 +0800

    [NEW]1.添加扩展方法 2.添加HeadView

---
+++ memodule/src/main/java/com/yang/mac/memodule/ui/activity/MyPayTypeActivity.kt
@@ -0,0 +1,13 @@
+package com.yang.mac.memodule.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.yang.mac.memodule.R
+
+class MyPayTypeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_pay_type)
+    }
+}
