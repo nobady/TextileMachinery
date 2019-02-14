720817e9813131c9ad6a535bcad864899e26932e
tengfei_lv <tengfei_lv@kingdee.com>
2019-02-14 17:54:11 +0800

    改了一些东西，哈哈哈，忘记是啥了

--- base/src/main/java/com/game/base/wdget/LoadingDialog.kt
+++ base/src/main/java/com/game/base/wdget/LoadingDialog.kt
@@ -1,6 +1,7 @@
 package com.game.base.wdget
 
 import android.app.Dialog
+import android.content.Context
 import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
 import android.widget.ProgressBar
@@ -11,7 +12,7 @@ import com.github.ybq.android.spinkit.style.FadingCircle
  * @author ex-yangjb001
  * @date 2019/1/10.
  */
-class LoadingDialog(activity: FragmentActivity) : Dialog(activity, R.style.LoadingDialog) {
+class LoadingDialog(activity: Context) : Dialog(activity, R.style.LoadingDialog) {
     init {
         val view = LayoutInflater.from(activity).inflate(R.layout.loading, null, false)
         setContentView(view)
